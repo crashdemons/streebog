0581b6ec5f2dc6788fede4181109a0f52f743f4d
crashdemons <crashenator@gmail.com>
2021-02-21 22:18:43 -0600

    set optimization for EMCC

--- make-wasm.sh
+++ make-wasm.sh
@@ -1,4 +1,4 @@
 rm -rf streebog-wasm-dist
 mkdir streebog-wasm-dist
-emcc -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' gost3411-2012-core.c streebog-wasm-lib.c -o streebog-wasm-dist/streebog-wasm.js
+emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' gost3411-2012-core.c streebog-wasm-lib.c -o streebog-wasm-dist/streebog-wasm.js
 
