5d92e6624ac5b4cd031d222c39d82f4235198a8b
crashdemons <crashenator@gmail.com>
2021-02-21 23:59:13 -0600

    modularize streebog loader to prevent polluting global namespace as much

--- make-wasm.sh
+++ make-wasm.sh
@@ -1,4 +1,4 @@
 rm -rf streebog-wasm-dist
 mkdir streebog-wasm-dist
-emcc -O3 -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' gost3411-2012-core.c streebog-wasm-lib.c -o streebog-wasm-dist/streebog-wasm.js
+emcc -O3 -s MODULARIZE=1 -s -s 'EXPORT_NAME="createStreebogModule"' -s WASM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' gost3411-2012-core.c streebog-wasm-lib.c -o streebog-wasm-dist/streebog-wasm.js
 
